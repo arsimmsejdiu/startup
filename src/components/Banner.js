add0aafde9b0eef8d3fa4360cc6a50619d799bee
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-05-10 16:55:10 +0200

    Designing the banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,15 @@
 import React, { useState, useEffect } from "react";
+import { motion } from 'framer-motion';
+
+// Variants 
+const banner = {
+  animate: {
+    transition: {
+      delayChildren: 0.4,
+      staggerChildren: 0.1
+    }
+  }
+};
 
 const Banner = () => {
   const [playMarquee, setPlayMarquee] = useState(false);
